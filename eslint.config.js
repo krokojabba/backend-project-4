8e2a27e2bcdc6730570ea553d046567dec684aee
krokojabba <krokojabba@gmail.com>
2024-05-09 15:39:04 +0300

    Fix bugs

--- eslint.config.js
+++ eslint.config.js
@@ -1,39 +1,37 @@
-import globals from "globals";
-
-import path from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-import pluginJs from "@eslint/js";
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import pluginJs from '@eslint/js';
 
 // mimic CommonJS variables -- not needed if using CommonJS
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended});
+const compat = new FlatCompat({ baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended });
 
 export default [
   {/* languageOptions: { globals: globals.node } */},
-/*   ...compat.extends("airbnb-base", "plugin:jest/recommended"),
+  /*   ...compat.extends("airbnb-base", "plugin:jest/recommended"),
   ...compat.env({ node: true }),
   ...compat.plugins("jest"), */
   ...compat.config({
-    plugins: ["jest"],
-    extends: ["airbnb-base", "plugin:jest/recommended"],
+    plugins: ['jest'],
+    extends: ['airbnb-base', 'plugin:jest/recommended'],
     env: {
-        node: true,
-        es2020: true,
+      node: true,
+      es2020: true,
     },
     rules: {
-      "import/extensions": 0,
-      "import/no-named-as-default": 0,
-      "import/no-named-as-default-member": 0
+      'import/extensions': 0,
+      'import/no-named-as-default': 0,
+      'import/no-named-as-default-member': 0,
     },
     parserOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module"
-    }
-}),
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+  }),
   {
-    ignores: ["eslint.config.js", 'temp.js', '__fixtures__/*'],
+    ignores: ['temp.js', '__fixtures__/*', 'eslint.config.js'],
   },
   /* {
     rules: {
